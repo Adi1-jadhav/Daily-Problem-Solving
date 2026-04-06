0705f651402623109bd97cb11bf9dddec82d78ca
Aditya Jadhav <adityajadhav3117@gmail.com>
2026-04-06 12:08:03 +0530

    Day 8: Move Zeroes using two pointers

---
+++ Day8_MoveZeroes.java
@@ -0,0 +1,27 @@
+class Day8_MoveZeroes {
+
+    public static void moveZeroes(int[] nums) {
+        int j = 0; // position for non-zero
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                // swap
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,0,3,12};
+
+        moveZeroes(nums);
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
